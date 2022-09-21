0031c368fe713759013d7f1c4cc4962aeb4c5ff9
jennysival <jeniffersival@gmail.com>
2022-09-21 14:04:38 -0300

    First commit

---
+++ app/src/main/java/br/com/jenny/appgames/MainActivity.kt
@@ -0,0 +1,11 @@
+gitpackage br.com.jenny.appgames
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
