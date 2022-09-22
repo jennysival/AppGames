9fd6ae47dfcdae52ba5f7f229b645079867482cb
sivalgabe <sival.gabriel@gmail.com>
2022-09-22 10:38:35 -0300

    Add game detail page

--- app/src/main/java/br/com/jenny/appgames/ui/gameDetail/DetailFragment.kt
+++ app/src/main/java/br/com/jenny/appgames/ui/gameDetail/DetailFragment.kt
@@ -5,16 +5,87 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import br.com.jenny.appgames.R
+import br.com.jenny.appgames.data.model.GameResult
+import br.com.jenny.appgames.databinding.FragmentDetailBinding
+import br.com.jenny.appgames.ui.home.HomeActivity
+import br.com.jenny.appgames.utils.GAME_KEY
+import com.squareup.picasso.Picasso
 
 class DetailFragment : Fragment() {
+    private lateinit var binding: FragmentDetailBinding
+    private lateinit var game: GameResult
+
+    private val viewModel: DetailViewModel by lazy {
+        ViewModelProvider(this)[DetailViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    ): View {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        getGame()
+        setUpView()
+        setUpSaveButton()
+    }
+
+    private fun getGame() {
+        val getGame = arguments?.getParcelable<GameResult>(GAME_KEY)
+
+        if (getGame != null) {
+            game = getGame
+        } else {
+            Toast.makeText(context, "Não foi possível carregar o jogo", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setUpView() {
+        game.let {
+            Picasso.get().load(it.backgroundImage).into(binding.ivGameDetail)
+            binding.tvGameNameDetail.text = it.name
+            binding.tvGameReleaseDetail.text = "Release: " + it.released
+            updateSaveIcon()
+
+            (activity as HomeActivity).supportActionBar?.title = it.name
+        }
+        (activity as HomeActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun setUpSaveButton() {
+        binding.iconSave.setOnClickListener {
+            game.saved = !game.saved
+            updateSaveIcon()
+            viewModel.updateSavedGame(game)
+            setUpSavedToast()
+        }
+    }
+
+    private fun updateSaveIcon() {
+        binding.iconSave.setImageDrawable(
+            ContextCompat.getDrawable(
+                binding.root.context,
+                if (game.saved) R.drawable.ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24
+            )
+        )
+    }
+
+    private fun setUpSavedToast(){
+        if(game.saved){
+            Toast.makeText(context, "${game.name} foi salvo com sucesso", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            Toast.makeText(context, "${game.name} foi excluído", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
